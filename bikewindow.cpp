5d59a6b0d0b7edcf450462d8e07d9756192bf349
aovadia <ovadia.adam2@gmail.com>
2017-06-20 19:14:15 -0400

    Add a method to get Bike data from the Bike ID

--- bikewindow.cpp
+++ bikewindow.cpp
@@ -1,10 +1,65 @@
 #include "bikewindow.h"
 
 #include <QVBoxLayout>
+#include <QDebug>
+#include <QMessageBox>
 
 bikeWindow::bikeWindow(QWidget *parent) : QWidget(parent)
 {
+    setupBikeWindow();
+}
+
+void bikeWindow::setupBikeWindow() {
     QVBoxLayout *myQVBox = new QVBoxLayout(this);
-    this->resize(1500,1000);
+    this->resize(400,400);
+
+    enterBikeID = new QLabel();
+    enterBikeID->setText("Enter Bike ID");
+    enterBikeID->setAlignment(Qt::AlignCenter);
+
+
+    myQVBox->addWidget(enterBikeID);
+
+    editBikeID = new QLineEdit();
+    editBikeID->setAlignment(Qt::AlignCenter);
+
+    myQVBox->addWidget(editBikeID);
+
+
+    acceptBikeID = new QPushButton();
+    acceptBikeID->setText("Enter");
+
+
+    myQVBox->addWidget(acceptBikeID);
+
+
     this->setLayout(myQVBox);
+
+    //When 'Enter' button is pressed, run 'checkBikeID'
+    connect(acceptBikeID, &QPushButton::released, this, &bikeWindow::checkBikeID);
+}
+
+void bikeWindow::checkBikeID() {
+    qDebug() <<"You pressed the accept button!";
+    bool isValidInput;
+    bikeID = editBikeID->text().toUInt(&isValidInput, 10);
+    qDebug() <<"Bike ID: " <<bikeID;
+    if (!isValidInput) {
+        QMessageBox::warning(this, "Error", "Invalid Character!");
+    } else {
+        // Check sql server for matching bike id
+
+        displayBikeInfo();
+    }
+
+
+}
+
+void bikeWindow::displayBikeInfo() {
+    this->resize(1500,1000);
+    // Delete objects from window
+    delete editBikeID;
+    delete acceptBikeID;
+    delete enterBikeID;
+
 }
--- bikewindow.h
+++ bikewindow.h
@@ -1,6 +1,9 @@
 #ifndef BIKEWINDOW_H
 #define BIKEWINDOW_H
 #include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
 
 
 class bikeWindow : public QWidget
@@ -8,6 +11,20 @@ class bikeWindow : public QWidget
     Q_OBJECT
 public:
     explicit bikeWindow(QWidget *parent = 0);
+    void setupBikeWindow();
+
+private slots:
+    void checkBikeID();
+
+private:
+    QLineEdit *editBikeID;
+    QLabel *enterBikeID;
+    QPushButton *acceptBikeID;
+
+    int bikeID;
+
+    void displayBikeInfo();
+
 };
 
 #endif // BIKEWINDOW_H
