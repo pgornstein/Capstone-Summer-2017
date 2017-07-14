9dd0574893b80cb0fd7fa295fc5beb05857d9a47
aovadia <ovadia.adam2@gmail.com>
2017-07-13 21:26:16 -0400

    Remove unnecessary message box

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -36,23 +36,9 @@ void MainWindow::on_loginButton_released()
     checkUser *isUPValid = new checkUser(ui->usernameTxt->text(), ui->passwordTxt->text());
     if (isUPValid->isAccountFound()) {
         qDebug() <<"Success!";
-        QMessageBox *EULA = new QMessageBox();
-        EULA->setText("This is the End User License Agreement \n Capstone does not come with warrenty!");
-        EULA->setStandardButtons(QMessageBox::Yes);
-        EULA->addButton(QMessageBox::No);
-        EULA->show();
-        EULA->setModal(true);
-        if (EULA->exec() == QMessageBox::Yes) {
-            // close current windows and open main app 'todo'
-            //QApplication::closeAllWindows();
-            hide();            
-            accountManage *myManage = new accountManage();
-            myManage->show();
-        } else {
-            // User did not agree to terms and conditions, quit app
-            QApplication::quit();
-        }
-
+        hide();
+        accountManage *myManage = new accountManage();
+        myManage->show();
     } else { /* Display error message on status bar */
         ui->statusBar->showMessage("Invalid Username and/or Password!", 3000);
         ui->passwordTxt->clear();
