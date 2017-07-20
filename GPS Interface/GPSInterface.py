8541d499d6e906e7c4cad466e62285c920749d3d
TasdiqueC <tasdiquec@gmail.com>
2017-07-20 18:17:30 -0400

    Updated GPS, made running distance, connected to server
    In this update, I changed the latitude and longitude to be more accurate (for example, it used to come out as a super 4047.9821, but now it is 40.7997016667), made the distance become a running one (in graph terms, we now have ab+bc+cd as opposed to just ab, ac, or ad), and connected to the AWS MySQL server and pushed the necessary data (latitude, longitude, distance, and date-time). The SQL server will also push a running distance to give a cumulative distance over the course of history of a bike’s life, so our distance now increments with that

--- GPS Interface/GPSInterface.py
+++ GPS Interface/GPSInterface.py
@@ -4,174 +4,124 @@
   displays the position of the rider every second, and calculates the distance
   traveled.
 """
-
-# Specification standard from: The National Marine Electronics Association (NMEA)
-
-# GGA Fix information
-# essential fix data which provides 3D location and accuracy data.
-
-##Where:
-##     GGA          Global Positioning System Fix Data
-##     123519       Fix taken at 12:35:19 UTC
-##     4807.038,N   Latitude 48 deg 07.038' N
-##     01131.000,E  Longitude 11 deg 31.000' E
-##     1            Fix quality: 0 = invalid
-##                               1 = GPS fix (SPS)
-##                               2 = DGPS fix
-##                               3 = PPS fix
-##              4 = Real Time Kinematic
-##              5 = Float RTK
-##                               6 = estimated (dead reckoning) (2.3 feature)
-##              7 = Manual input mode
-##              8 = Simulation mode
-##     08           Number of satellites being tracked
-##     0.9          Horizontal dilution of position
-##     545.4,M      Altitude, Meters, above mean sea level
-##     46.9,M       Height of geoid (mean sea level) above WGS84
-##                      ellipsoid
-##     (empty field) time in seconds since last DGPS update
-##     (empty field) DGPS station ID number
-##     *47          the checksum data, always begins with *
-
-##If the height of geoid is missing then the altitude should be suspect. Some non-standard implementations
-##report altitude with respect to the ellipsoid rather than geoid altitude. Some units do not report
-##negative altitudes at all. This is the only sentence that reports altitude.
-
 from geopy.distance import great_circle
 import serial
-import pynmea3
+import pynmea2
+from datetime import datetime
+import mysql.connector
+from mysql.connector import errorcode
 
+cnx = mysql.connector.connect(user='db_admin', password ='capstone',
+                              host='capstone-bikes.cphpxguj45gw.us-east-1.rds.amazonaws.com',
+                              database='Capstone_Bike_Shop')
+cursor = cnx.cursor()
+print("Connected to database...")
+
+bikeID = 1
+GGApat = " | Lat: {0:s} {1:s} | Lon: {2:s} {3:s} | Alt: {4:.4f} {5:s}"
+
+'''
+    PARSE THE GPS AND OBTAINED THE DATA OF A SINGLE MESSAGE FROM THE GPS
+'''
 def parseGPS(rawGpsData):
-  """ Args:
-          rawGpsData: A string received from the GPS reader.
+    
+    msg_ = ""
 
-      Returns:
-          If a valid string is passed,
-              returns a string containing time, latitude, longitude, and altitude.
-          Else,
-              returns the string "error\n".
-  """
-  msg_ = ""
-
-  if rawGpsData.find('GGA') > 0:
-      # msg is a Model that stores the parsed gps data
-      msg_ = pynmea3.parse(rawGpsData)  # parse raw gps coordinates
-      # msg = pynmea3.parse('$GNGGA,140816.00,,,,,,,,,,,,')
-
-  if msg_ == "":
-      return "error\n"
-  else:
-      return msg_
-
+    if rawGpsData.find('GGA') > 0:
+        # msg is a Model that stores the parsed gps data
+        msg_ = pynmea2.parse(rawGpsData) # parse raw gps coordinates
+        #msg = pynmea2.parse('$GNGGA,140816.00,,,,,,,,,,,,')
+        
+    if msg_ == "":
+        return "error\n"
+    else:
+        return msg_
+        
+'''
+    RETURNS THE LATITUDE AND LONGITUDE, WHICH IS NEEDED TO CALCULATE THE DISTANCE
+'''
 def getCurrentPosition(msg):
-  """
-  Args:
-      msg: A string containing time, latitude, longitude, and altitude.
 
-  Returns:
-      A pair containing the latitude and the longitude as doubles.
-      This pair is used later to calculate the distance traveled by the rider.
-  """
-  GGApat = " | Lat: {0:s} {1:s} | Lon: {2:s} {3:s} | Alt: {4:.4f} {5:s}"
+    latLonAltString = str(GGApat.format(msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
 
-  #The format function edits the GGApat string
-  #It plugs in latitude as argument 0, longitude as arguments 2 and 3, and altitude as arguments 4 and 5
-  latLonAltString = str(GGApat.format(msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
+    latLonAltList = latLonAltString.split('|')
 
-  latLonAltList = latLonAltString.split('|')
+    latitude = latLonAltList[1]
+    longitude = latLonAltList[2]
 
-  latitude = latLonAltList[1]
-  longitude = latLonAltList[2]
+    latitudeList = latitude.split(' ')
+    longitudeList = longitude.split(' ')
 
-  latitudeList = latitude.split(' ')
-  longitudeList = longitude.split(' ')
+    latitude = latitudeList[2]
+    longitude = longitudeList[2]
 
-  latitude = latitudeList[2]
-  longitude = longitudeList[2]
+    # makes the degrees, minutes, and seconds, into a single degree float
+    latitude = float(latitude[:2]) + float(latitude[2:9])/60
+    longitude = float(longitude[:3]) + float(longitude[3:10])/60
 
-  latLon = (float(latitude), float(longitude))
+    # gives the the lat and lon as a pair
+    latLon = (latitude, longitude)
+    return latLon 
+    
+'''
+    PRINTS THE GPS COORDINATES AND THE DISTANCE TRAVELED BY THE RIDER
+'''
+def printGPSdata(ending_point, dateTimeStamp, totalDistance):
+    datetime = 'Date-Time: %s' %dateTimeStamp
+    print (datetime + " | Lat: %s" %str(ending_point[0]) + " | Lon: %s" %str(ending_point[1]))
+    print("Distance Traveled: " + totalDistance + " kilometers\n")
 
-  return latLon
+'''
+    GETS DISTANCE GIVEN TWO PAIRS OF LAT-LON POINTS
+    HELPS FOR INCREMENTING THE DISTANCE
+'''
+def getDistance(starting_point, ending_point):
+    distance = great_circle(starting_point, ending_point).kilometers
+    return distance #returns distance between two points, helps increment total distance
 
-def editString(gpsData):
-  """Helper function for the printGPSdata and the getCurrentPosition functions
-      Args:
-          gpsData: A semi-legible string containing the latitude, longitude,
-          and altitude.
-
-      Returns:
-          A more legible string.
-  """
-  # separate altitude off the string
-  removedBarsList = gpsData.split("|")
-  altitude = "|" + removedBarsList[3]
-  latLon = gpsData[:-len(altitude)]
-
-  # add 'd' for degrees and 'm' for minutes
-  count = 0
-  for x in latLon:
-
-      if x == '.':
-          latLon = latLon[:count - 2] + " d, " + latLon[count - 2:count + 6] + "m, " + latLon[count + 6:]
-          count = count + 7
-      count = count + 1
-
-  # remove any unnecessary leftmost zeroes
-  count = 0
-  for x in latLon:
-      zero = latLon[count + 2:count + 3]
-      if x == ":" and zero == "0":
-          latLon = latLon[:count + 2] + latLon[count + 3:]
-      count = count + 1
-
-  # reattach altitude and return the edited string
-  editedString = latLon + altitude
-  return editedString
-
-def printGPSdata(msg, starting_point, ending_point):
-"""Prints the GPS coordinates and the distance traveled by the rider"""
-  time = 'Time: %s' % (msg.timestamp)
-  latLonAlt = GGApat.format(msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units)
-  print(time + editString(latLonAlt))
-  # print('Time: %s' %(msg.timestamp) + GGApat.format(msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
-
-  #great_circle formula takes in two rider positions as two pairs of
-  #latitude and longitude and calculates the distance between these two points
-  distance = great_circle(starting_point, ending_point).kilometers
-  distance = str(distance)
-  print("distance traveled: " + distance + " kilometers\n")
-
+'''
+    RUNS MYSQL QUERIES GIVEN THE PROPER INPUTS
+'''
+def updateToTable(bikeID, ending_point, datetime, totalDistance):
+    currentLat = ending_point[0]
+    currentLon = ending_point[1]
+    cursor.execute("INSERT INTO Locations (BikeId, Latitude, Longitude, Timestamp) VALUES (%s, %s, %s, %s) ", (bikeID,currentLat,currentLon,datetime,))
+    cnx.commit()
+    cursor.execute("UPDATE Capstone_Bike_Shop.Master SET Distance = %s WHERE BikeId = 1 ", (totalDistance,))
+    cnx.commit()
+    
+    
 def main():
-  starting_point = ""
-  ending_point = ""
-  serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
-  while True:
-  #gets the start position of the rider's trip.
-  #loops until a valid "GGA" string is read.
-      rawGps = serialPort.readline()
-     msg = parseGPS(rawGps)
+    starting_point = ""
+    ending_point = ""
+    serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
-      if msg == "error\n":
-          pass  # print(msg)
-      else:
-          starting_point = getCurrentPosition(msg)
-          ending_point = starting_point
-          printGPSdata(msg, starting_point, ending_point)
-          break
+    # totalDistance = 0.0
+    while True:
+        rawGps = serialPort.readline()
+        msg = parseGPS(rawGps)
 
-  while True:
-  #reads the rider's position every second.
-  #loops until the user terminates the program.
-      rawGps = serialPort.readline()
-      msg = parseGPS(rawGps)
-
-      if msg == "error\n":
-          pass  # print(msg)
-      else:
-          ending_point = getCurrentPosition(msg)
-          printGPSdata(msg, starting_point, ending_point)
+        # obtains running distance from MySQL server
+        cursor.execute("SELECT Distance FROM Master WHERE BikeId = %s ", (bikeID,))
+        totalDistance = cursor.fetchone()
+    
+        if msg == "error\n":
+            print("Seeking...")
+            pass
+        else:
+            i = datetime.now()
+            dateTimeStamp = i.strftime('%Y-%m-%d %H:%M:%S')
+            if (starting_point == ""):
+                starting_point = getCurrentPosition(msg)
+                ending_point = starting_point
+            elif (starting_point != ""):
+                starting_point = ending_point
+                ending_point = getCurrentPosition(msg)
+                totalDistance += getDistance(starting_point, ending_point)
+            printGPSdata(ending_point, dateTimeStamp, str(totalDistance))
+            updateToTable(bikeID, ending_point, dateTimeStamp, totalDistance)
 
 main()
-
-
+cursor.close()
+cnx.close()
